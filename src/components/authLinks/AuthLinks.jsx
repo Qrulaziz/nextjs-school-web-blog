477476a76e0ba5958f1dc318a984cb45f328d9cc
qrulaziz <azizsahrul41@gmail.com>
2023-10-30 20:22:03 +0700

    write page, login page

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -8,7 +8,7 @@ const AuthLinks = () => {
 
   const [open, setOpen] = useState(false);
 
-  const status = "notauthenticated"
+  const status = "authenticated"
   return <>
   {status === "notauthenticated" ? (
     <Link href="/login" className={styles.link}>Login</Link>
