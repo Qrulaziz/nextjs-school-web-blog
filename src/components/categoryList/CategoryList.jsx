a000aa7befcab13de45c5c430e9ea251d379ede1
qrulaziz <azizsahrul41@gmail.com>
2023-11-01 21:41:27 +0700

    prisma schema, category, posts, routing

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -3,35 +3,32 @@ import styles from './categoryList.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 
-const CategoryList = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Gagal")
+  }
+
+  return res.json()
+}
+
+const CategoryList = async () => {
+  const data = await getData();
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Pilih Kategori</h1>
       <div className={styles.categories}>
-          <Link href="/blog?cat=pendidikan" className={`${styles.category} ${styles.pendidikan}`}>
-            <Image src="/style.png" alt="" width={32} height={32} className={styles.image}/>
-            pendidikan
-          </Link>
-          <Link href="/blog?cat=ekskul" className={`${styles.category} ${styles.ekskul}`}>
-            <Image src="/fashion.png" alt="" width={32} height={32} className={styles.image}/>
-            ekskul
-          </Link>
-          <Link href="/blog?cat=kurikulum" className={`${styles.category} ${styles.kurikulum}`}>
-            <Image src="/food.png" alt="" width={32} height={32} className={styles.image}/>
-            kurikulum
-          </Link>
-          <Link href="/blog?cat=teknologi" className={`${styles.category} ${styles.teknologi}`}>
-            <Image src="/travel.png" alt="" width={32} height={32} className={styles.image}/>
-            teknologi
-          </Link>
-          <Link href="/blog?cat=p5" className={`${styles.category} ${styles.p5}`}>
-            <Image src="/culture.png" alt="" width={32} height={32} className={styles.image}/>
-            p5
-          </Link>
-          <Link href="/blog?cat=lomba" className={`${styles.category} ${styles.lomba}`}>
-            <Image src="/coding.png" alt="" width={32} height={32} className={styles.image}/>
-            lomba
-          </Link>
+          { data?.map((item) => (
+            <Link href="/blog?cat=pendidikan" className={`${styles.category} ${styles[item.slug]}`} key={item._id}>
+              { item.img && (<Image src={item.img} alt="" width={32} height={32} className={styles.image}/>)}
+            {item.title}
+            </Link>
+          )) 
+          }
       </div>
     </div>
   )
