b59712ddc76bfeb9c633ab35e9bed42b1e8eea8f
qrulaziz <azizsahrul41@gmail.com>
2023-11-06 18:44:42 +0700

    write page,

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,12 +1,16 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./writePage.module.css"
 import Image from 'next/image'
 import ReactQuill from 'react-quill'
 import "react-quill/dist/quill.bubble.css"
 import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { app } from '@/utils/firebase'
+
+const storage = getStorage(app);
 
 const Write = () => {
   const {status} = useSession()
@@ -14,25 +18,92 @@ const Write = () => {
   const router = useRouter()
 
   const [open, setOpen] = useState(false)
+  const [file, setFile] = useState(null)
+  const [media, setMedia] = useState("")
   const [value, setValue] = useState("")
+  const [title, setTitle] = useState("")
+
+  useEffect(() => {
+    const upload = () => {
+      const name = new Date().getTime + file.name
+      const storageRef = ref(storage, name);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on('state_changed', 
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+          }
+        }, 
+        (error) => {
+          // Handle unsuccessful uploads
+        }, 
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setMedia(downloadURL);
+          });
+        }
+      );
+    }
+    file && upload()
+  }, [file])
 
   if (status === "loading") {
     return <div className={styles.loading}>Loading...</div>
   }
-  if (status === "authenticated") {
+  if (status === "unauthenticated") {
     router.push("/")
   }
+
+  const slugify = (str) => 
+    str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/[\s_-]+/g, "-")
+    .replace(/^-+|-+/g, "");
+  
+
+  const handleSubmit = async () => {
+    const res = await fetch("/api/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        desc: value,
+        img: media,
+        slug: slugify(title),
+        catSlug: "pendidikan",
+      })
+    })
+
+    console.log(res)
+  }
+
   return (
     <div className={styles.container}>
-      <input type="text" placeholder='Judul' className={styles.input}/>
+      <input type="text" placeholder='Judul' className={styles.input} onChange={e => setTitle(e.target.value)}/>
+      {/* <input type="text" placeholder="kategori" /> */}
       <div className={styles.editor}>
         <button className={styles.button} onClick={() => setOpen(!open)}>
           <Image src="/plus.png" alt="" width={16} height={16} />
         </button>
         {open && <div className={styles.add}>
-          <button className={styles.addButton}>
-            <Image src="/image.png" alt="" width={16} height={16} />
-          </button>
+          <input type="file" id="image"onChange={e => setFile(e.target.files[0])} style={{display: "none"}}/>
+            <button className={styles.addButton}>
+              <label htmlFor="image">
+                <Image src="/image.png" alt="" width={16} height={16} />
+              </label>
+            </button>
           <button className={styles.addButton}>
             <Image src="/external.png" alt="" width={16} height={16} />
           </button>
@@ -42,7 +113,7 @@ const Write = () => {
         </div>}
         <ReactQuill className={styles.textArea} theme="bubble" value={value} onChange={setValue} placeholder="Tuliskan beritamu..." />
       </div>
-      <button className={styles.posting}>Posting</button>
+      <button className={styles.posting} onClick={handleSubmit}>Posting</button>
     </div>
   )
 }
--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,51 +1,61 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import styles from "./comment.module.css"
 import Link from 'next/link'
 import Image from 'next/image'
+import useSWR from 'swr'
+import { useSession } from 'next-auth/react'
 
-const Comments = () => {
+const fetcher = async (url) => {
+    const res = await fetch(url)
 
-  const status = "authenticated"  
+    const data = await res.json();
+
+    if (!res.ok) {
+        const error = new Error(data.message);
+        throw error;
+    }
+
+    return data;
+}
+
+const Comments = ({postSlug}) => {
+
+  const {status} = useSession()
+  
+  const {data, mutate, isLoading} = useSWR(`http://localhost:3000/api/comments?postSlug=${postSlug}`, fetcher)
+
+  const [desc, setDesc] = useState("")
+
+  const handleSubmit = async () => {
+    await fetch("/api/comments", {
+        method: "POST",
+        body: JSON.stringify({desc, postSlug})
+    });
+    mutate();
+  }
+  
   return (
     <div className={styles.container}>
         <h1 className={styles.title}>Komentar</h1>
         {status === "authenticated" ? (<div className={styles.write}>
-            <textarea placeholder='Tulis komentar...' className={styles.textInput} />
-            <button className={styles.button}>Kirim</button>
+            <textarea placeholder='Tulis komentar...' className={styles.textInput} onChange={e => setDesc(e.target.value)}/>
+            <button className={styles.button} onClick={handleSubmit}>Kirim</button>
         </div>) : (<Link href="/login">Login untuk menulis komentar</Link>)}
         <div className={styles.comments}>
-            <div className={styles.comment}>
-                <div className={styles.user}>
-                    <Image src="/p1.jpeg" alt="" width={50} height={50} className={styles.image} />
-                    <div className={styles.userInfo}>
-                        <span className={styles.username}>Sahrul Aziz</span>
-                        <span className={styles.date}>01.11.2023</span>
-                    </div>
-                </div>
-                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolorem, aperiam minima autem excepturi, laudantium quidem, repellendus iusto architecto quod pariatur ab? Nulla at temporibus officiis, enim eius nesciunt perferendis.</p>
-            </div>
-
-            <div className={styles.comment}>
-                <div className={styles.user}>
-                    <Image src="/p1.jpeg" alt="" width={50} height={50} className={styles.image} />
-                    <div className={styles.userInfo}>
-                        <span className={styles.username}>Sahrul Aziz</span>
-                        <span className={styles.date}>01.11.2023</span>
-                    </div>
-                </div>
-                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolorem, aperiam minima autem excepturi, laudantium quidem, repellendus iusto architecto quod pariatur ab? Nulla at temporibus officiis, enim eius nesciunt perferendis.</p>
-            </div>
-
-            <div className={styles.comment}>
-                <div className={styles.user}>
-                    <Image src="/p1.jpeg" alt="" width={50} height={50} className={styles.image} />
-                    <div className={styles.userInfo}>
-                        <span className={styles.username}>Sahrul Aziz</span>
-                        <span className={styles.date}>01.11.2023</span>
-                    </div>
-                </div>
-                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolorem, aperiam minima autem excepturi, laudantium quidem, repellendus iusto architecto quod pariatur ab? Nulla at temporibus officiis, enim eius nesciunt perferendis.</p>
-            </div>
+            {isLoading ? "loading" : data?.map(item => (
+                 <div className={styles.comment} key={item._id}>
+                 <div className={styles.user}>
+                     {item?.user?.image && <Image src={item.user.image} alt="" width={50} height={50} className={styles.image} />}
+                     <div className={styles.userInfo}>
+                         <span className={styles.username}>{item.user.name}</span>
+                         <span className={styles.date}>{item.createdAt.substring(0, 10)}</span>
+                     </div>
+                 </div>
+                 <p className={styles.desc}>{item.desc}</p>
+             </div>
+            ))}
         </div>
     </div>
   )
