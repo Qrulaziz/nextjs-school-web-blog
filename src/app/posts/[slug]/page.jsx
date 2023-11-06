f98c43ede966a4d028a0d8823d3a7758bda11254
qrulaziz <azizsahrul41@gmail.com>
2023-11-06 20:45:02 +0700

    domain setting pt2

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -6,7 +6,7 @@ import Comments from '@/components/comments/Comments'
 
 
 const getData = async (slug) => {
-  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+  const res = await fetch(`https://smp4-dongko.vercel.app/api/posts/${slug}`, {
     cache: "no-store",
   });
 
